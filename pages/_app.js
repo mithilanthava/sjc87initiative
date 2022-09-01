977446204f793aecf1c0be50687a877bfd7ccf40
ahadnur <ahadnur44@gmail.com>
2022-09-02 01:49:16 +0600

    Favicon Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 import '../styles/globals.css'
 import '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <Header />
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       <Footer />
     </>
   )
